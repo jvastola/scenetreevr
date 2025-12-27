3369c618a6b3788d934fddeb1fb1c6867d3efdef
Johnathan Vastola <32178472+jvastola@users.noreply.github.com>
2025-12-26 16:22:57 -0800

    Update workspace_repo/src/resources/inventory_item.gd

---
+++ workspace_repo/src/resources/inventory_item.gd
@@ -0,0 +1,9 @@
+class_name InventoryItem
+extends Resource
+
+@export var id: String = ""
+@export var name: String = "New Item"
+@export_multiline var description: String = ""
+@export var icon: Texture2D
+@export var max_stack: int = 1
+@export var value: int = 0
